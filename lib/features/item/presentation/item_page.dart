dd2c8e95e3f2fa5f8707ac86a1b5e5ca17b5ea71
GiacomoGuidotto <giacomo.guidotto@gmail.com>
2022-04-13 09:52:43 +0200

    Add items page header

--- lib/features/item/presentation/item_page.dart
+++ lib/features/item/presentation/item_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:museo_zuccante/core/colors.dart';
 import 'package:museo_zuccante/features/item/presentation/bloc/item_bloc.dart';
 import 'package:museo_zuccante/models/item.dart';
 
@@ -22,7 +23,6 @@ class _ItemPageState extends State<ItemPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: BlocConsumer<ItemBloc, ItemState>(
         listener: (context, state) {
           if (state is ItemError) {
@@ -44,8 +44,24 @@ class _ItemPageState extends State<ItemPage> {
   }
 
   Widget _buildLoaded(Item item) {
-    return Center(
-      child: Text(item.title),
+    return CustomScrollView(
+      slivers: [
+        SliverPersistentHeader(
+          pinned: true,
+          delegate: HeaderDelegate(item),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              return Container(
+                alignment: Alignment.center,
+                color: Colors.lightBlue[100 * (index % 9)],
+                child: Text('List Item $index'),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 
@@ -54,4 +70,77 @@ class _ItemPageState extends State<ItemPage> {
       child: CircularProgressIndicator(),
     );
   }
+}
+
+class HeaderDelegate extends SliverPersistentHeaderDelegate {
+  final Item item;
+
+  HeaderDelegate(this.item);
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    final progress = shrinkOffset / maxExtent;
+    return Material(
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          AnimatedOpacity(
+            duration: const Duration(milliseconds: 150),
+            opacity: progress,
+            child: const ColoredBox(
+              color: Palette.primary,
+            ),
+          ),
+          AnimatedOpacity(
+            opacity: 1 - progress,
+            duration: const Duration(milliseconds: 150),
+            child: Image.network(
+              item.poster,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned.fill(
+            child: Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.bottomLeft,
+                      end: Alignment.topCenter,
+                      colors: [Colors.black, Colors.transparent])),
+            ),
+          ),
+          Positioned(
+              top: 10,
+              left: 10,
+              child: IconButton(
+                icon: const Icon(Icons.arrow_back, color: Colors.white),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )),
+          Positioned(
+            bottom: 10,
+            left: 10,
+            child: Text(
+              item.title,
+              style: const TextStyle(fontSize: 32, color: Colors.white),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 400;
+
+  @override
+  double get minExtent => 100;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      true;
 }
