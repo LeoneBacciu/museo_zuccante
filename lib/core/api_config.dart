f2a28f9c21fb954ca0e9ca16fcc772fe11ed6124
LeoneBacciu <leonebacciu@gmail.com>
2022-04-13 08:39:04 +0200

    Add baseUrl

--- lib/core/api_config.dart
+++ lib/core/api_config.dart
@@ -1,12 +1,12 @@
 class ApiConfig {
-  static const baseUrl = '';
-  static const roomsUrl = 'rooms';
-  static const itemsUrl = 'items';
-  static const companiesUrl = 'company';
+  static const baseUrl = 'http://10.42.0.124/api/';
+  static const roomsUrl = 'rooms/';
+  static const itemsUrl = 'items/';
+  static const companiesUrl = 'company/';
 
-  static roomUrl(String id) => 'room/$id';
+  static roomUrl(String id) => 'room/$id/';
 
-  static itemUrl(String id) => 'item/$id';
+  static itemUrl(String id) => 'item/$id/';
 
-  static companyUrl(String id) => 'company/$id';
+  static companyUrl(String id) => 'company/$id/';
 }
--- lib/core/core_container.dart
+++ lib/core/core_container.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:museo_zuccante/core/api_config.dart';
 import 'package:museo_zuccante/features/home/home_container.dart';
 import 'package:museo_zuccante/features/item/item_container.dart';
 
@@ -9,7 +10,7 @@ final sl = GetIt.instance;
 class CoreContainer {
   static Future<void> init() async {
     sl.registerLazySingleton<Dio>(
-      () => Dio(BaseOptions(baseUrl: 'http://localhost/api/')),
+      () => Dio(BaseOptions(baseUrl: ApiConfig.baseUrl)),
     );
     await HomeContainer.init();
     await ItemContainer.init();
